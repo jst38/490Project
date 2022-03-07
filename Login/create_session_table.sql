fef6ad5ac085125e62e0e19fa6a6a9a670f127f5
DCherdak <56012603+DCherdak@users.noreply.github.com>
2022-03-07 17:25:43 -0500

    da

--- Login/create_session_table.sql
+++ Login/create_session_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Session_Tokens (
 	Session_ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	Session_Create_Date DATETIME2,
-	Session_Start_Time DATETIME2,
-	Session_End_Time DATETIME2,
+	Session_Create_Date DATETIME,
+	Session_Start_Time DATETIME,
+	Session_End_Time DATETIME,
 	Username VARCHAR(60) NOT NULL
   	);

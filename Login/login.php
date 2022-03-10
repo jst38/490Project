6cd55095e2e0da1b6d1aa1fe6ba7e94e7b8870ca
DCherdak <56012603+DCherdak@users.noreply.github.com>
2022-03-10 13:56:05 -0500

    da

---
+++ Login/login.php
@@ -0,0 +1,13 @@
+#!/usr/bin/php
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+
+function login($username,$password)
+{
+    // lookup username in databas
+    // check password
+    return true;
+    //return false if not valid
+}

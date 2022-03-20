e1490f6d3ff47fb771edd6a3dda91b8561ad7bdf
DCherdak <dc545@njit.edu>
2022-03-20 19:54:30 -0400

    ?? soo vendor got loaded in...

---
+++ Login/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '5de7f709411f6ecbb168d90b1e62a43f0e344f33',
+        'name' => 'vendor/autoload.php',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor/autoload.php' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '5de7f709411f6ecbb168d90b1e62a43f0e344f33',
+            'dev_requirement' => false,
+        ),
+        'videlalvaro/php-amqplib' => array(
+            'pretty_version' => 'v2.5.2',
+            'version' => '2.5.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../videlalvaro/php-amqplib',
+            'aliases' => array(),
+            'reference' => 'eb8f94d97c8e79900accf77343dbd7eca7f58506',
+            'dev_requirement' => false,
+        ),
+    ),
+);

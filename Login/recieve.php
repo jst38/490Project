10d3cb8d5be56e8f5adc7311ae8f4353ac9b94ce
DCherdak <dc545@njit.edu>
2022-03-20 17:09:05 -0400

    adding AMQP info

--- Login/recieve.php
+++ Login/recieve.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 
-$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+$connection = new AMQPStreamConnection('192.168.191.251', 5672, 'Denise', 'caff1');
 $channel = $connection->channel();
 
 $channel->queue_declare('hello', false, false, false, false);

4c3649f8c23b633cdb6380f368851defbadaf5ca
mo282 <87154046+mo282@users.noreply.github.com>
2022-04-28 12:14:35 -0400

    Update register.php

--- website/register.php
+++ website/register.php
@@ -7,8 +7,8 @@ require_once(__DIR__ .'/path.inc');
 require_once(__DIR__ .'/get_host_info.inc');
 require_once(__DIR__ .'/RabbitMQLib.inc');
 
-if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 1800)) {
-  // last request was more than 30 minutes ago
+if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 60)) {
+  // the last request was more than 1 minutes ago
   session_unset();     // unset $_SESSION variable for the run-time 
   session_destroy();   // destroy session data in storage
 }

df40bccef50f635994c5dd91104b6089b2acdc7a
DCherdak <56012603+DCherdak@users.noreply.github.com>
2022-05-03 12:26:23 -0400

    Update create_session_table.sql

--- mysql/create_session_table.sql
+++ mysql/create_session_table.sql
@@ -1,8 +1,5 @@
 CREATE TABLE Session_Tokens (
 	Session_ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	User_ID Int NOT NULL
-	Session_Create_Date DATETIME,
-	Session_Start_Time DATETIME,
-	Session_End_Time DATETIME,
 	FOREIGN KEY (User_ID) REFERENCES Users(User_ID)
   	);

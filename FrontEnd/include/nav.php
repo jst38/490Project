2dd9d21b011833973bc3fc7592543e6ece79509c
DCherdak <dc545@njit.edu>
2022-05-08 22:09:39 -0400

    changed ini files to Rabbit Server

--- FrontEnd/include/nav.php
+++ FrontEnd/include/nav.php
@@ -11,18 +11,10 @@ require_once(__DIR__ . "/../lib/functions.php");
         <li><a href="register.php">Register</a></li>
     <?php endif; ?>
     <?php if (has_role("Admin")): ?> 
-            <li><a href="test_create_accounts.php">Create Account</a></li>
-            <li><a href="test_list_accounts.php">Accounts</a></li>
-            <li><a href="test_create_transactions.php">Create Transactions</a></li>
-            <li><a href="deposit.php">Deposit</a></li>
-            <li><a href="withdraw.php">Withdraw </a></li>
-            <li><a href="transfer.php">Transfer</a></li>
-            <li><a href="transaction_history.php">View Transactions</a></li>
-            <li><a href="external_transfer.php">Extranal Tranfer</a></li>
-        <?php endif; ?>
-    <?php if (is_logged_in()): ?>
-        <li><a href="profile.php">Profile</a></li>
-        <li><a href="logout.php">Logout</a></li>
+            <li><a href="search.php">Search Receipes</a></li>
+            <li><a href="bookmark.php">Bookmarked Receipes</a></li>
+            <li><a href="fridge.php">My Fridge</a></li>
+             <li><a href="logout.php">Logout</a></li>
     <?php endif; ?>
 </ul>
 </nav>

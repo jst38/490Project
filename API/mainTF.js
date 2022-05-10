68404576c196339351cdac5e309ff02519d3497c
jst38 <60358093+jst38@users.noreply.github.com>
2022-05-10 12:02:52 -0400

    search..j

--- API/mainTF.js
+++ API/mainTF.js
@@ -5,6 +5,23 @@ let searchQuery = "";
 const APP_ID = "ba090757";
 const APP_key = "daa20634d8472a5e8d63e245bfe22c58";
 
+let star = document.querySelectorAll('input');
+let showValue = document.querySelector('#rating-value');
+
+for (let i = 0; i < star.length; i++) {
+	star[i].addEventListener('click', function() {
+		i = this.value;
+
+		showValue.innerHTML = i + " out of 5";
+	});
+}
+
+
+
+
+
+
+
 searchForm.addEventListener("submit", (e) => {
     e.preventDefault();
     searchQuery = e.target.querySelector("input").value;
@@ -41,7 +58,25 @@ function generateHTML(results){
             <p class="item-data"><b><li>Protein</li></b> ${result.recipe.totalNutrients.PROCNT.quantity.toFixed(2)}</p>
             </ul>
 
+            <div class="rating-wrap">
+			<h2>Rating</h2>
+			<div class="center">
+				<fieldset class="rating">
+					<input type="radio" id="star5" name="rating" value="5"/><label for="star5" class="full" title="Awesome"></label>
+					<input type="radio" id="star4.5" name="rating" value="4.5"/><label for="star4.5" class="half"></label>
+					<input type="radio" id="star4" name="rating" value="4"/><label for="star4" class="full"></label>
+					<input type="radio" id="star3.5" name="rating" value="3.5"/><label for="star3.5" class="half"></label>
+					<input type="radio" id="star3" name="rating" value="3"/><label for="star3" class="full"></label>
+					<input type="radio" id="star2.5" name="rating" value="2.5"/><label for="star2.5" class="half"></label>
+					<input type="radio" id="star2" name="rating" value="2"/><label for="star2" class="full"></label>
+					<input type="radio" id="star1.5" name="rating" value="1.5"/><label for="star1.5" class="half"></label>
+					<input type="radio" id="star1" name="rating" value="1"/><label for="star1" class="full"></label>
+					<input type="radio" id="star0.5" name="rating" value="0.5"/><label for="star0.5" class="half"></label>
+				</fieldset>
+			</div>
 
+			<h4 id="rating-value"></h4>
+		</div>
             
         </div>
         `

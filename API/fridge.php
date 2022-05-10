7d24bc03fb9896b12a791edec020d72e1136be39
johnathan thompson <jst38@njit.edu.com>
2022-05-09 22:16:44 -0400

    last

---
+++ API/fridge.php
@@ -0,0 +1,16 @@
+<!Doctype html>
+    <head>
+    <title>Fridge Content</title>
+    </head>
+<body>
+            <h1 align="center"> Your Fridge </h1>
+            <form action="index.php" method="post">
+                <h5 align="center"> Items in Fridge: <input type="text" name="items">
+                <input type="submit">
+                </h5>
+            </form>
+            <br>
+        <?php echo you have $_POST["items"]; ?>
+</body>
+  
+</html>
